a9b15237f30a245b7e1a8560a4bd05d9dd31ce77
navisayslisten <1cakeisalie@gmail.com>
2020-11-29 00:20:46 -0700

    v0.2.3

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -5901,36 +5901,34 @@ __webpack_require__(/*! ace-builds/webpack-resolver */ "./node_modules/ace-build
 __webpack_require__(/*! ace-builds/src-min-noconflict/ext-language_tools */ "./node_modules/ace-builds/src-min-noconflict/ext-language_tools.js");
 __webpack_require__(/*! ace-builds/src-min-noconflict/ext-error_marker */ "./node_modules/ace-builds/src-min-noconflict/ext-error_marker.js");
 ace.config.set("basePath", "modules/AceEditorVTT/scripts/");
+const furnaceMessage = 'Ace Editor VTT is not compatible with Furnace. Ace is now disabled, in favor of Furnace.';
 
 
-Hooks.on("renderMacroConfig", function (aceConfig) {
+Hooks.once('ready', function() {
+    furnaceUICheck();
+});
+
+Hooks.once('init', function () {
+    game.AceEditorVTT= {};
+    CONFIG.debug.AceEditorVTT = false;
+    _settings__WEBPACK_IMPORTED_MODULE_0__.AceSettings.init();
+});
+
+Hooks.on('renderMacroConfig', function (aceConfig) {
     const enabled = game.settings.get('AceEditorVTT', 'enabled');
     const fontSize = game.settings.get('AceEditorVTT', 'fontSize');
     // const themeName = game.settings.get('AceEditorVTT', 'theme');
     // let theme = '';
     const autoComplete = game.settings.get('AceEditorVTT', 'autoComplete');
     const errorCheck = game.settings.get('AceEditorVTT', 'errorCheck');
-
     const configElement = aceConfig.element;
 
-    if (!enabled) {
-        configElement.find(".ace-editor").css("display", "none");
-        return;
-    }
-
-    // TODO: Figure out how to detect Furnace. For some reason this is true even if Furnace isn't installed. Weird.
-    /*if (configElement.has(".furnace-macro-command")) {
-        const message = 'Ace Editor VTT is not compatible with Furnace. Disabling Ace in favor of Furnace.';
-        ui.notifications.error(message);
-        console.error(message)
-        game.settings.set('AceEditorVTT', 'enabled', false)
-            .then(r => console.info(`AEVTT now set to ${r}`));
-        return;
-    }*/
-
+    // If not enabled, or if Furnace is enabled, bail!
+    if (!enabled) return configElement.find('.ace-editor').css('display', 'none');
+    if (furnaceUICheck()) return;
 
     configElement
-        .find("div.form-group.stacked.command")
+        .find('div.form-group.stacked.command')
         .append(
             `<div class="ace-editor" id="aceEditor-${aceConfig.object.id}"></div>`
         );
@@ -5950,54 +5948,59 @@ Hooks.on("renderMacroConfig", function (aceConfig) {
 
     configElement.find('.command textarea[name="command"]').css("display", "");
     configElement
-        .find(".sheet-footer")
+        .find('.sheet-footer')
         .append('<button type="button" class="ace-editor-button" title="Toggle Ace Editor" name="editorButton"><i class="fas fa-terminal"></i></button>');
 
     let editor = ace.edit(`aceEditor-${aceConfig.object.id}`);
     editor.getSession().setUseWorker(errorCheck);
     editor.setOptions({
-        mode: "ace/mode/javascript",
+        mode: 'ace/mode/javascript',
         fontSize: `${fontSize}pt`,
         showPrintMargin: false,
-        foldStyle: "markbegin",
+        foldStyle: 'markbegin',
         enableBasicAutocompletion: autoComplete,
         enableSnippets: autoComplete,
         enableLiveAutocompletion: autoComplete,
         minLines: 15,
         maxLines: 50,
     });
-    editor.setTheme("ace/theme/solarized_dark");
-    editor.getSession().setUseWrapMode(game.settings.get("AceEditorVTT", "lineWrap"));
+    editor.setTheme('ace/theme/solarized_dark');
+    editor.getSession().setUseWrapMode(game.settings.get('AceEditorVTT', 'lineWrap'));
 
-    configElement.find(".ace-editor-button").on("click", (event) => {
+    configElement.find('.ace-editor-button').on('click', (event) => {
         event.preventDefault();
-        if (configElement.find(".ace-editor").css("display") === "none") {
+        if (furnaceUICheck()) {
+            editor.container.remove()
+            editor.destroy()
+            return;
+        }
+        if (configElement.find('.ace-editor').css("display") === "none") {
             configElement.find('.command textarea[name="command"]').css("display", "none");
-            configElement.find(".ace-editor").css("display", "");
+            configElement.find('.ace-editor').css("display", "");
             editor.setValue(configElement.find('.command textarea[name="command"]').val(), -1);
         } else {
             configElement.find('.command textarea[name="command"]').css("display", "");
-            configElement.find(".ace-editor").css("display", "none");
+            configElement.find('.ace-editor').css("display", "none");
         }
     });
 
     configElement.find('.command textarea[name="command"]').css("display", "");
-    configElement.find(".ace-editor").css("display", "none");
+    configElement.find('.ace-editor').css("display", "none");
 
-    editor.getSession().on("change", () => {
+    editor.getSession().on('change', () => {
         configElement.find('textarea[name="command"]').val(editor.getSession().getValue());
     });
 
     editor.commands.addCommand({
-        name: "Save",
-        bindKey: { win: "Ctrl-S", mac: "Command-S" },
-        exec: () => configElement.find("form.editable").trigger("submit"),
+        name: 'Save',
+        bindKey: { win: 'Ctrl-S', mac: 'Command-S' },
+        exec: () => configElement.find('form.editable').trigger('submit'),
     });
 
     editor.commands.addCommand({
-        name: "Execute",
-        bindKey: { win: "Ctrl-E", mac: "Command-E" },
-        exec: () => configElement.find("button.execute").trigger("click"),
+        name: 'Execute',
+        bindKey: { win: 'Ctrl-E', mac: 'Command-E' },
+        exec: () => configElement.find('button.execute').trigger('click'),
     });
 
     // watch for resizing of editor
@@ -6010,20 +6013,33 @@ Hooks.on("renderMacroConfig", function (aceConfig) {
 });
 
 function createMacroConfigHook(id, editor) {
-    Hooks.once("closeMacroConfig", function (aceConfig) {
+    Hooks.once('closeMacroConfig', function (aceConfig) {
         if (id === aceConfig.id) {
             editor.destroy();
+            editor.container.remove()
         } else {
             createMacroConfigHook(id, editor);
         }
     });
 }
 
-Hooks.once('init', function () {
-    game.AceEditorVTT= {};
-    CONFIG.debug.AceEditorVTT = false;
-    _settings__WEBPACK_IMPORTED_MODULE_0__.AceSettings.init();
-})
+function furnaceUICheck() {
+    const aceEnabled = game.settings.get('AceEditorVTT', 'enabled');
+    if (!aceEnabled) return;
+    if ($('div.furnace-macro-expand').length > 0 || $('link[href*="furnace"]').length > 0) {
+        ui.notifications.error(furnaceMessage);
+        console.error(furnaceMessage)
+        disableAce();
+        $('a.close').trigger('click');
+        return true;
+    }
+    return false;
+}
+
+function disableAce() {
+    game.settings.set('AceEditorVTT', 'enabled', false)
+        .then(r => console.debug(`AEVTT now set to ${r}`));
+}
 
 /***/ }),
 
